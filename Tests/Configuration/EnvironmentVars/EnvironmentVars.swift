47078887611b0f9057bfa136897eb2a720fa1dc5
James Newkirk <newkirk.jim@gmail.com>
2025-03-29 18:23:35 -0700

    Delete Tests/Configuration/EnvironmentVars directory
    fix Xcode issue

--- Tests/Configuration/EnvironmentVars/EnvironmentVars.swift
+++
@@ -1,9 +0,0 @@
-// Generated using Sourcery 2.2.6 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-
-//swiftlint:disable all
-struct EnvironmentVars {
-    /// Generated using 'EnvironmentVars.stencil' template
-    static let togglePublicKey = "public_b3JnXzY3YmY4NjhjMDYwOGU2YTdmMWY0YjkwNzpwcm9qXzY3YmY4NjhkMDYwOGU2YTdmMWY0YjkwZDpGNUFQbU9PbFQyaHpCTVdnOWxZZA=="
-}
-
